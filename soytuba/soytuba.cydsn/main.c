bd3082ae71d441379ab9a594cef18a48fefd41e1
K <kamiyaowl@gmail.com>
2019-04-07 21:17:52 +0900

    DMAとりあえず作った

--- soytuba/soytuba.cydsn/main.c
+++ soytuba/soytuba.cydsn/main.c
@@ -1,10 +1,21 @@
 #include "project.h"
+
+#include "shared_mem.h"
 #include "ymf825.h"
 
-int main(void) {
+uint16_t slider_value = 0;
+
+void init() {
+    shared_mem_init();
+    
     ymf825_init();
     ymf825_set_tone();
     ymf825_set_ch();
+    
+    ADC_SLIDER_AMux_Start();
+ }
+int main(void) {
+    init();
     
     CyGlobalIntEnable; 
 
---
+++ soytuba/soytuba.cydsn/shared_mem.h
@@ -0,0 +1,12 @@
+#include "project.h"
+
+#define SHARED_MEM_PRESSURE_N (3)
+
+struct shared_mem_t {
+    uint32_t adc_slider;
+    uint32_t adc_pressure[SHARED_MEM_PRESSURE_N];
+};
+volatile struct shared_mem_t shared_mem;
+
+void shared_mem_dma_init();
+void shared_mem_init();
