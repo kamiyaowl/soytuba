98820f7626d670b89a734bf12511235a6796d927
K <kamiyaowl@gmail.com>
2019-04-09 01:04:40 +0900

    IMUをI2CからSPIに置き換え

--- soytuba/soytuba.cydsn/bmi160.h
+++ soytuba/soytuba.cydsn/bmi160.h
@@ -1,13 +1,49 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+#ifndef BMI160_H
+#define BMI160_H
 
-/* [] END OF FILE */
+#include "project.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// SPI porting
+inline void bmi160_spi_cs(uint8_t assert) {
+    REG_SS_YM825_Write(assert ? 0x0 : 0x1);
+}
+inline void bmi160_delay(uint32_t ms){
+    CyDelay(ms);
+}
+inline void bmi160_spi_wait_done(){
+    uint8_t data;
+    do{
+        data = SPIM_BMI160_ReadTxStatus();
+    } while(!(data & SPIM_BMI160_STS_SPI_DONE));
+}
+inline void bmi160_spi_transfer(const uint8_t* tx_data, uint8_t* rx_data, uint8_t length) {
+    SPIM_BMI160_ClearFIFO();
+    
+    bmi160_spi_cs(0x1);
+    for(uint32_t i = 0 ; i < length ; ++i) {
+        SPIM_BMI160_WriteTxData(tx_data[i]);
+        uint8_t rx = SPIM_BMI160_ReadRxData();
+        if (rx_data != NULL) {
+            rx_data[i] = rx;
+        }
+        bmi160_spi_wait_done();
+    }
+    bmi160_spi_cs(0x0);
+}
+inline void bmi160_spi_write_reg(uint8_t addr, uint8_t data) {
+    uint8_t datas[] = {addr, data};
+    bmi160_spi_transfer(datas, NULL, 2);
+}
+inline void bmi160_spi_init() {
+    bmi160_spi_cs(0x0);
+    SPIM_BMI160_Start();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Application
+// from https://github.com/yamaha-webmusic/bmi160board/blob/master/sample1/bmi160board_sample1/bmi160board_sample1.ino
+
+void bmi160_init();
+
+#endif
