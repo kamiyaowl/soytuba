691d6105604e0a1134a196241c18dee269b2cd00
K <kamiyaowl@gmail.com>
2019-04-10 00:55:47 +0900

    bmi160のchip_idよめた

--- soytuba/soytuba.cydsn/bmi160.c
+++ soytuba/soytuba.cydsn/bmi160.c
@@ -21,17 +21,22 @@ void bmi160_init(){
 }
 
 void bmi160_update() {
-    // uint8_t write_buf[13] = { 0x0c }; // 0x0c: GYR_X[7:0], 0x0d: GYRO_X[15:8]...
-    uint8_t write_buf[13] = { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0xa5, 0x5a, 0x3c, 0xc3 }; // 0x0c: GYR_X[7:0], 0x0d: GYRO_X[15:8]...
-    uint8_t read_buf[13] = {}; // 先頭はダミーが入る
-    
-    bmi160_spi_transfer(write_buf, read_buf, 13);
-    
-    shared_mem.gyro[0] = (read_buf[1] & 0xff) | ((uint16_t)read_buf[2] << 8);
-    shared_mem.gyro[1] = (read_buf[3] & 0xff) | ((uint16_t)read_buf[4] << 8);
-    shared_mem.gyro[2] = (read_buf[5] & 0xff) | ((uint16_t)read_buf[6] << 8);
-    
-    shared_mem.acc[0] = (read_buf[7]  & 0xff) | ((uint16_t)read_buf[8]  << 8);
-    shared_mem.acc[1] = (read_buf[9]  & 0xff) | ((uint16_t)read_buf[10] << 8);
-    shared_mem.acc[2] = (read_buf[11] & 0xff) | ((uint16_t)read_buf[12] << 8);
+    uint8_t write_buf[2] = { 0x00 | 0x80 }; // 0x0c: GYR_X[7:0], 0x0d: GYRO_X[15:8]...
+    uint8_t read_buf[2] = {}; // 先頭はダミーが入る
+
+    bmi160_spi_transfer(write_buf, read_buf, 2);
+    shared_mem.gyro[0] = read_buf[1];
+//    // read cmdは最初のアドレスバイトの先頭を立てる
+//    uint8_t write_buf[13] = { 0x0c | 0x80 }; // 0x0c: GYR_X[7:0], 0x0d: GYRO_X[15:8]...
+//    uint8_t read_buf[13] = {}; // 先頭はダミーが入る
+//    
+//    bmi160_spi_transfer(write_buf, read_buf, 13);
+//    
+//    shared_mem.gyro[0] = (read_buf[1] & 0xff) | ((uint16_t)read_buf[2] << 8);
+//    shared_mem.gyro[1] = (read_buf[3] & 0xff) | ((uint16_t)read_buf[4] << 8);
+//    shared_mem.gyro[2] = (read_buf[5] & 0xff) | ((uint16_t)read_buf[6] << 8);
+//    
+//    shared_mem.acc[0] = (read_buf[7]  & 0xff) | ((uint16_t)read_buf[8]  << 8);
+//    shared_mem.acc[1] = (read_buf[9]  & 0xff) | ((uint16_t)read_buf[10] << 8);
+//    shared_mem.acc[2] = (read_buf[11] & 0xff) | ((uint16_t)read_buf[12] << 8);
 }
